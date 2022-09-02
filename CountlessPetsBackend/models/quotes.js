1936546057d70e697329bb706c5f09e7c3841385
pattisoj <joshpattison23@icloud.com>
2022-09-02 13:36:27 +0100

    Added validation

--- CountlessPetsBackend/models/quotes.js
+++ CountlessPetsBackend/models/quotes.js
@@ -1,42 +1,54 @@
 import { query } from "../db/index.js";
 import price from "./price.js";
+import validation from "./validate.js";
 
 // GET all quotes
 
 export async function getAllQuotes() {
-    const data = await query(`SELECT * FROM quotes`)
-    return data.rows;
+  const data = await query(`SELECT * FROM quotes`);
+  return data.rows;
 }
 
 // GET quote by ID
 export async function getQuotebyId(id) {
-    const data = await query(
-      `SELECT * FROM quotes WHERE id = $1;`,
-      [id]
-    );
-    return data.rows[0];
-  }
+  const data = await query(`SELECT * FROM quotes WHERE id = $1;`, [id]);
+  return data.rows[0];
+}
 
 // Get price for Quote POST a quote REPLACE WITH CORRECT DATA
-export async function createQuote(newQuote) { 
-  console.log(newQuote)
-    const data = await query(
-      `INSERT INTO quotes (pet_type, pet_breed, pet_age, gender, name, address, city, postcode, number_of_pets) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9) RETURNING *;`,
-      [newQuote.pet_type, newQuote.pet_breed, newQuote.pet_age, newQuote.gender, newQuote.name,  newQuote.address, newQuote.city, newQuote.postcode, newQuote.number_of_pets ]
-    );
-    let newQuotePrice = price(newQuote)
-
-    return data.rows[0];
+export async function createQuote(newQuote) {
+  console.log(newQuote);
+  const data = await query(
+    `INSERT INTO quotes (pet_type, pet_breed, pet_age, gender, name, address, city, postcode, number_of_pets) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9) RETURNING *;`,
+    [
+      newQuote.pet_type,
+      newQuote.pet_breed,
+      newQuote.pet_age,
+      newQuote.gender,
+      newQuote.name,
+      newQuote.address,
+      newQuote.city,
+      newQuote.postcode,
+      newQuote.number_of_pets,
+    ]
+  );
+  let validationResponse = validation(newQuote);
+  if (validationResponse[0] === false || validationResponse[1] === false) {
+    return `We couldn't validate your address and dog breed please try again.`;
   }
+
+  let newQuotePrice = price(newQuote);
+  let priceOfQuote = `£` + newQuotePrice;
+
+  return `Your quote for insurance is ${priceOfQuote} paid monthly at ${
+    priceOfQuote / 12
+  }`;
+}
 
 // DELETE a quote by ID
 export async function deleteQuoteById(id) {
-    const data = await query(
-      `DELETE FROM quotes WHERE id = $1 RETURNING *;`, 
-      [id]
-    );
-    return data.rows[0];
-  }
-
-
-
+  const data = await query(`DELETE FROM quotes WHERE id = $1 RETURNING *;`, [
+    id,
+  ]);
+  return data.rows[0];
+}
---
+++ CountlessPetsBackend/models/validate.js
@@ -0,0 +1,47 @@
+import fetch from "node-fetch";
+
+export default async function validation(newQuote) {
+  let validatedPostcode = await validatePostcode(newQuote);
+  let validatedBreed = await validateDogBreed(newQuote);
+
+  return [validatedPostcode, validatedBreed];
+}
+
+async function validatePostcode(newQuote) {
+  const postcode = newQuote.postcode;
+
+  let response = await fetch(`https://api.postcodes.io/postcodes/${postcode}`);
+  const data = await response.json();
+
+  //console.log("outside", validatedPostcode);
+  if (data.status === 200) {
+    return true;
+    //console.log("inside", validatedPostcode);
+  }
+  return false;
+}
+
+async function validateDogBreed(newQuote) {
+  let dogList = [];
+
+  const enteredBreed = newQuote.pet_breed.toLowerCase();
+
+  let response = await fetch(`https://api.thedogapi.com/v1/breeds/`);
+  const data = await response.json();
+
+  data.map((breed) => {
+    let dogBreed = breed.name.toLowerCase();
+
+    dogList.push(dogBreed);
+  });
+
+  //   console.log("outside", validatedBreed);
+  if (dogList.includes(enteredBreed)) {
+    return true;
+    // console.log("inside", validatedBreed);
+  }
+
+  return false;
+}
+
+//[newQuote.pet_type, newQuote.pet_breed, newQuote.pet_age, newQuote.gender, newQuote.name,  newQuote.address, newQuote.city, newQuote.postcode, newQuote.number_of_pets ]
